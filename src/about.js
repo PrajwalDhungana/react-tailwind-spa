536b4f42dd2328110b635b0d69dc0975253da4f9
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2022-12-11 18:07:25 +0545

    Add page

---
+++ src/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="flex justify-center items-center m-auto flex-col p-20">
+      <div>
+      <span className="uppercase text-4xl font-bold text-slate-700">About</span>
+      </div>
+    </div>
+  );
+}
