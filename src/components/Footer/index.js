d839156c7dc8223fb7b13943261a01ee9ea0abc4
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2022-12-15 16:24:43 +0545

    refactor: Add components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function footer() {
+export default function Footer() {
   return (
     <footer className="absolute  bottom-0 text-center w-full px-8 py-5 text-slate-500 bg-gray-100">
-      <span>&copy; Copyright</span>
+      <span>Made with ❤️</span>
     </footer>
   );
 }
